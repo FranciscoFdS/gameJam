f0bd21eadf31cfd1261256dbd0a9605e8279a5f8
Heitor Cardoso Freitas <heitorcafr@gmail.com>
2026-09-22 17:46:38 -0300

    up

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const correio = document.getElementById('task')
 const close = document.getElementById('close')
 correio.addEventListener('click', () => { modal.style.display = 'block'})
 close.addEventListener('click', () => { modal.style.display = 'none'})
+
+const button1 = document.getElementsByClassName("m1")
+const button2 = document.getElementsByClassName("m2")
+const button3 = document.getElementsByClassName("m3") 
+
+var dia = 1
+
+button1.addEventListener("click", () => {
+    dia += 1
+})
